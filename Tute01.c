f6deb72003592b665698afef18811013781e308a
it22890528 <127614771+IT22890528@users.noreply.github.com>
2023-03-11 16:53:26 +0000

    Exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+  int mark1, mark2, total, avg;
+  
+  printf ("Enter mark: ");
+  scanf (%d,&mark1);
+  printf ("Enter mark: ");
+  scanf (%d,&mark2);
+
+  total=markk1+mark2;
+  avg=total/2;
+
+  printf("Total marks: %d\n",total);
+  printf("Average marks: %d",,avg);
   
   return 0;
 }
